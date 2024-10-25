7df4ef28c815780b5a09fb8e1c1e7776fd1eb28b
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-10-25 07:50:39 -0500

    feat: :sparkles: new multimedia content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/auth/Login';
+import CreateAccount from './components/auth/CreateAccount';
+import Home from './components/home/Home';
+import CreateNote from './components/note/CreateNote';
+import EditNote from './components/note/EditNote';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/createAccount" element={<CreateAccount />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/createNote" element={<CreateNote />} />
+        <Route path="/editNote" element={<EditNote />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
