85e10ca314ef966c7a0ebd6af1c675a4a145565f
Edwin <egarciapame@gmail.com>
2024-10-27 17:57:22 -0500

    feat: :sparkles: homepage creation

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,24 +1,36 @@
-// src/components/home/Home.jsx
-import React from 'react';
+import React from 'react'
+import rafiki from '../../assets/img/rafiki.png';
+import search from '../../assets/img/search.png';
+import info_outline from '../../assets/img/info_outline.png';
 
-const Home = () => {
+import { Plus } from 'lucide-react'
+
+export default function EmptyHomeScreen() {
   return (
-    <div className="h-screen w-screen flex flex-col items-center text-white">
-      <h1 className="font-poppins font-bold mb-10 text-3xl">Home Page</h1>
-      <input
-        type="text"
-        className="border border-slate-0 rounded-lg px-4 py-2 mb-4 bg-color1 text-black"
-        placeholder="Search notes..."
-      />
-      <div className="w-full flex flex-col items-center">
-        {/* Aquí irán las notas, por ahora un ejemplo de nota */}
-        <div className="bg-gray-800 p-4 rounded-lg mb-4 w-1/2">
-          <h2 className="font-semibold">Nota de Ejemplo</h2>
-          <p>Descripción de la nota de ejemplo.</p>
+    <div className="flex flex-col h-screen text-white">
+      <header className="flex justify-between items-center p-4 pt-12">
+        <h1 className="text-5xl font-semibold">Notes</h1>
+        <div className="flex space-x-2">
+          <button className="w-12 h-12 justify-items-center rounded-2xl bg-[#3B3B3B] hover:bg-gray-800 transition-colors duration-200">
+          <img src={search} className='w-4 h-4' alt="Imagen de busqueda" />
+          </button>
+          <button className="w-12 h-12 justify-items-center rounded-2xl bg-[#3B3B3B] hover:bg-gray-800 transition-colors duration-200">
+          <img src={info_outline} className='w-4 h-4' alt="Imagen de busqueda" />
+          </button>
         </div>
-      </div>
-    </div>
-  );
-};
+      </header>
+      <main className="flex-grow flex flex-col items-center p-8 justify-center">
+        <div className="w-full mb-6 justify-items-center">
+        <img src={rafiki} alt="Imagen de ejemplo" />
+        </div>
+        <p className="text-lg mb-6">Create your first note !</p>
+      </main>
+      <footer className="p-4">
+  <button className="w-16 h-16 bg-[#252525] float-right text-black hover:bg-gray-200 rounded-full font-medium flex items-center justify-center transition-colors duration-200 shadow-[8px_0px_15px_rgba(0,0,0,0.5)]">
+    <Plus className="text-white h-5 w-5" />
+  </button>
+</footer>
 
-export default Home;
+    </div>
+  )
+}
