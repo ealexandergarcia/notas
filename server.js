bb44f3f04c8a0c52b4c1a124a8f5042c2ce3b45d
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-10-28 07:51:36 -0500

    feat: ✨ session validation with jwt v2

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const Database = require('./server/helper/db/connect');
 const privateKey = fs.readFileSync('private.key');
 const certificate = fs.readFileSync('certificate.crt');
 const { jsonParseErrorHandler } = require('./server/middleware/errorHandler'); // Importar el manejador de errores
-const  session  = require('./server/middleware/sessionConfig'); // Importar el manejador de errores
-
-
+const  SessionService  = require('./server/middleware/sessionConfig'); // Importar el manejador de errores
+const verifyJwt = require('./server/middleware/decodedJWT')
 // Importar las rutas de notas
 const noteRoutes = require('./server/router/noteRouters');
 const userRoutes = require('./server/router/userRouters');
@@ -18,7 +17,13 @@ const PORT_BACKEND = 5000;
 
 
 const app = express();
-app.use(session)
+
+// Middleware para habilitar CORS para el puerto 3000
+app.use(cors({
+    origin: ['https://localhost:3000'],
+    credentials: true // if you're using cookies or sessions
+}));
+
  // Llama para inicializar la conexión
 Database.getInstance();
 
@@ -28,24 +33,18 @@ const options = {
     cert: certificate // Ruta a tu certificado
 };
 
-// Middleware para habilitar CORS para el puerto 3000
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://localhost:3000'],
-    credentials: true // if you're using cookies or sessions
-}));
-
 
 // Middleware para parsear JSON
 app.use(express.json());
-
+SessionService.initializeSession(app);
 // Ruta de prueba
 app.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
 });
 
 // Agregar las rutas de notas
-app.use('/api/notes', noteRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/notes',verifyJwt, noteRoutes);
 
 // Configuración de manejo de errores globales
 app.use(jsonParseErrorHandler);
