f1a375f7e81dabed7997c0bde6b8eb9cc0e58950
Edwin <egarciapame@gmail.com>
2024-10-27 19:32:03 -0500

    feat: :sparkles: responsive upgrade

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -24,16 +24,13 @@ const Login = () => {
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center text-white">
-      <button>
-        <img className="flex mb-14 ml-72" src={smallStarImg} alt="Logo" />
-      </button>
+    <div className="w-full h-screen flex flex-col justify-center items-center text-white p-4">
       <h1 className="font-poppins font-bold mb-10 text-3xl">Log in</h1>
-      <form>
-        <div className="relative mb-4 w-80 text-sm">
+      <form className="w-full max-w-xs">
+        <div className="relative mb-4 w-full text-sm">
           <p className="font-inter font-regular mb-1.5 text-left">Email address</p>
           <input
-            className="border border-slate-0 rounded-lg px-4 py-2 w-80 bg-color1 text-black text-left bg-white"
+            className="border border-slate-0 rounded-lg px-4 py-2 w-full bg-color1 text-black"
             type="email"
             name="email"
             value={formData.email}
@@ -48,10 +45,10 @@ const Login = () => {
             alt="Validation"
           />
         </div>
-        <div className="relative mb-3.5 w-80 text-sm">
+        <div className="relative mb-3.5 w-full text-sm">
           <p className="font-inter font-regular mb-1.5 text-left">Password</p>
           <input
-            className="border border-slate-0 rounded-lg px-4 py-2 w-80 bg-color1 text-black text-left bg-white"
+            className="border border-slate-0 rounded-lg px-4 py-2 w-full bg-color1 text-black"
             type={passwordVisible ? 'text' : 'password'}
             name="password"
             value={formData.password}
@@ -67,24 +64,24 @@ const Login = () => {
             onClick={togglePasswordVisibility}
           />
         </div>
-        <p className="mb-9 ml-48 text-sm">Forgot password?</p>
-        <button className="bg-[#FE0000] font-inter font-semibold text-slate-50 px-36 py-4 rounded-lg mb-9">
+        <p className="mb-4 text-sm text-right">Forgot password?</p>
+        <button className="bg-[#FE0000] font-inter font-semibold text-slate-50 px-6 py-3 rounded-lg mb-5 w-full">
           Log in
         </button>
       </form>
-      <div className="flex flex-row mb-5 gap-x-2.5">
+      <div className="flex flex-row mb-5 gap-x-2 items-center">
         <img src={LineImg} alt="Line" />
         <p className="text-sm">Or Login with</p>
         <img src={LineImg} alt="Line" />
       </div>
       <div className="flex flex-row mb-32 gap-x-4">
-        <button className="border border-slate-0 rounded-lg px-11 py-4 w-28 bg-color1 bg-white">
+        <button className="border border-slate-0 rounded-lg px-4 py-2 w-14 bg-color1 bg-white flex justify-center items-center">
           <img className="w-6" src={facebookImg} alt="Facebook" />
         </button>
-        <button className="border border-slate-0 rounded-lg px-11 py-4 w-28 bg-color1 bg-white">
+        <button className="border border-slate-0 rounded-lg px-4 py-2 w-14 bg-color1 bg-white flex justify-center items-center">
           <img className="w-6" src={googleImg} alt="Google" />
         </button>
-        <button className="border border-slate-0 rounded-lg px-11 py-4 w-28 bg-color1 bg-white">
+        <button className="border border-slate-0 rounded-lg px-4 py-2 w-14 bg-color1 bg-white flex justify-center items-center">
           <img className="w-6" src={appleImg} alt="Apple" />
         </button>
       </div>
@@ -92,7 +89,7 @@ const Login = () => {
         <p className="font-inter font-regular text-sm">Don’t have an account?</p>
         <a
           className="font-inter font-semibold text-white border-b-2 border-slate-950 text-sm"
-          href="#"
+          href="/createAccount"
         >
           Sign up
         </a>
