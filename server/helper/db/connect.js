52d61e6e95a3441339a558029777b26da1b13a01
Edwin <egarciapame@gmail.com>
2024-10-29 23:02:33 -0500

    feat: :sparkles: cambio de base de datos

--- server/helper/db/connect.js
+++ server/helper/db/connect.js
@@ -8,13 +8,14 @@ class Database {
   _connect() {
     mongoose
       .connect(
-        `${process.env.MONGO_PROTOCOLO}${process.env.MONGO_USER}:${process.env.MONGO_PSW}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/notas`
+        `${process.env.MONGO_DB}`
       )
       .then(() => {
-        console.log(`${process.env.MONGO_PROTOCOLO}${process.env.MONGO_USER}:${process.env.MONGO_PSW}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/notas`);
+        console.log(`${process.env.MONGO_DB}`);
         console.log("Conectado correctamente a la base de datos");
       })
       .catch((err) => {
+        console.log(`${process.env.MONGO_DB}`);
         console.error("Error al conectar a la base de datos", err);
       });
   }
