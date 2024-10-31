150c6c62aeef2eecda06970b075990ed047658e9
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-10-31 10:24:08 -0500

    feat: :sparkles: correcion de tokenjwt

--- server/middleware/tokenJwt.js
+++ server/middleware/tokenJwt.js
@@ -7,12 +7,9 @@ class JwtService {
   static generateToken(payload) {
     const expiresIn = 30 * 60 * 1000;
     return jwt.sign(payload, secret, { expiresIn });
-    const expiresIn = 30 * 60 * 1000;
-    return jwt.sign(payload, secret, { expiresIn });
   }
 
   static verifyToken(token) {
-    return jwt.verify(token, secret);
     return jwt.verify(token, secret);
   }
 }
