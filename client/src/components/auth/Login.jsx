ba9750a6edfa78435ee0e1eac062ad30c02aac7d
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-10-31 10:46:18 -0500

    feat: :sparkles: correcion de imagenes

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import agreeImg from '../../assets/img/agree.svg';
 import disagreeImg from '../../assets/img/disagree.svg';
+import occultImg from '../../assets/img/occult.svg';
 // Importar imágenes como en tu código original
 
 const Login = () => {
