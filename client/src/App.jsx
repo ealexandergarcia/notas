eb23f6e4e6d8ab5368479cd80c09455b2a540ad5
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-10-31 09:58:59 -0500

    feat: ✨ cambio en las rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/createAccount" element={<CreateAccount />} />
+        <Route path="/notas/#/createAccount" element={<CreateAccount />} />
         
         {/* Rutas protegidas */}
         <Route 
